d9843e45e0a520a070f55521cf828ef79844cc4c
fhlxc <z15881614642@163.com>
2020-03-21 16:11:46 +0800

    实验一完成

--- 作业/实验1-静态白盒测试/FindbugsTest/src/com/fhlxc/test/FindbugsTest.java
+++ 作业/实验1-静态白盒测试/FindbugsTest/src/com/fhlxc/test/FindbugsTest.java
@@ -1,7 +1,6 @@
 package com.fhlxc.test;
 
 import java.security.SecureRandom;
-import java.util.Random;
 
 /**
 * @author Xingchao Long
@@ -75,6 +74,58 @@ public class FindbugsTest {
         System.out.println(str.equals(strb.toString()));
     }
     
+    public static void myfunc1(Object object) {
+        if (object == null) {
+            return;
+        }
+        System.out.println(object.toString());
+    }
+    
+    public static void myfunc2(int i) {
+        if (i == 4) {
+            return;
+        }
+        System.out.println(i);
+        myfunc2(i + 1);
+    }
+    
+    public static void myfunc3(int x) {
+        if (x == 0) {
+            System.out.println("true");
+        }
+    }
+    /*
+    @Override
+    public boolean equals(Object object) {
+        return object.toString().equals("sd") ? true: false;
+    }
+    */
+    public static void myfunc4() {
+        Object a = new Object();
+        System.out.println(a);
+    }
+    
+    public static void myfunc5() {
+        Object o = null;
+        Object object = o;
+        if (object == null) {
+            return;
+        }
+    }
+    
+    public static void myfunc6() {
+        String s = "  d";
+        s = s.trim();
+    }
+    
+    public static void myfunc7() {
+        System.out.println(new Object().equals(new Object()));
+    }
+    static int fool;
+    public static void myfunc8(int fool) {
+        FindbugsTest.fool = fool;
+    }
+    
     public static void main(String[] args) {
         func1();
         func2();
@@ -85,6 +136,11 @@ public class FindbugsTest {
         func7(10);
         //func8();
         func9();
+        myfunc1(null);
+        myfunc2(0);
+        myfunc3(0);
+        myfunc4();
+        myfunc5();
     }
 
 }
